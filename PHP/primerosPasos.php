f08da7ecfed3385fa20de347bedcc7ced5238d65
OskarAPP <al070144@uacam.mx>
2024-03-04 23:49:48 -0600

    Uso de if, else, for, while, do-while, swtich.

--- PHP/primerosPasos.php
+++ PHP/primerosPasos.php
@@ -1,3 +1,13 @@
 <?php
+//primer hola mundo
 echo "Hola mundo";
+
+//Uso de if, else, for, while, do-while, swtich.
+// las variables se definen con el simbolo de dolares "$".
+$edad = 18;
+if($edad >= 18){
+    echo "Eres mayor de edad";
+}   else{
+        echo "Eres menor de edad";
+    }
 ?>
